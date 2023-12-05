6bdff5ece6e5f5994d6c0db7a56d9779a988ec83
Michelle Primiani <primianimichelle@gmail.com>
2023-12-05 14:12:57 -0600

    created all fixtures

--- seed_database.sh
+++ seed_database.sh
@@ -5,8 +5,11 @@ rm -rf ./pixoapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations pixoapi
 python3 manage.py migrate pixoapi
-python3 manage.py loaddata categories
-python3 manage.py loaddata conditions
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
 python3 manage.py loaddata pixousers
+python3 manage.py loaddata images
+python3 manage.py loaddata collectibles
+python3 manage.py loaddata categories
+python3 manage.py loaddata collectiblecategories
+python3 manage.py loaddata imagegalleries
