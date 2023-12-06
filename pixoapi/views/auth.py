b3680791620b7d9132b320ba7b32919206fa1abe
Michelle Primiani <primianimichelle@gmail.com>
2023-12-06 09:59:09 -0600

    Updated auth

--- pixoapi/views/auth.py
+++ pixoapi/views/auth.py
@@ -4,6 +4,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework import status
 from pixoapi.models import PixoUser
 
 
@@ -30,11 +31,11 @@ def login_user(request):
             'token': token.key,
             'user_id': authenticated_user.id
         }
-        return Response(data)
+        return Response(data, status=status.HTTP_200_OK)
     else:
         # Bad login details were provided. So we can't log the user in.
         data = {'valid': False}
-        return Response(data)
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
