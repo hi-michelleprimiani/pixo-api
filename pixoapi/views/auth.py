79fa5054175b9166383fd6ecb76e79f4dda308c2
Michelle Primiani <primianimichelle@gmail.com>
2023-12-13 11:05:46 -0600

    updated description model to take more characters, added userId to auth

--- pixoapi/views/auth.py
+++ pixoapi/views/auth.py
@@ -67,5 +67,5 @@ def register_user(request):
     # Use the REST Framework's token generator on the new user account
     token = Token.objects.create(user=new_user)
     # Return the token to the client
-    data = {'token': token.key}
+    data = {'token': token.key, 'user_id': new_user.id}
     return Response(data)
---
+++ pixoapi/views/cart_view.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+
+
+class CartView(ViewSet):
+    pass
